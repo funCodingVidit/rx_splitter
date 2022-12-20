824a15d1cfc1421a72fdcd90de4880732902dafe
Vidit Khandelwal <vidit8290@gmail.com>
2022-12-20 11:54:56 +0530

    Created controller and add group screen

---
+++ lib/ui/screens/groups/add_group_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddGroupScreen extends StatefulWidget {
+  const AddGroupScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AddGroupScreen> createState() => _AddGroupScreenState();
+}
+
+class _AddGroupScreenState extends State<AddGroupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
